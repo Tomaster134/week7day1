0e5c21c34658162fcf8a09c66ac30f031a946045
Thomas Makurat <68624804+Tomaster134@users.noreply.github.com>
2024-02-20 12:30:14 -0500

    Update homework.js

--- homework.js
+++ homework.js
@@ -84,3 +84,20 @@ function squareDigits(num) {
   }
   return Number(newString);
 }
+
+// 3. Multiples of 3 or 5
+function solution(number) {
+  let sum = 0
+  for (let i = 0; i < number; i++) {
+    if (i % 3 == 0 && i % 5 == 0) {
+      sum += i;
+    }
+    else if (i % 5 == 0) {
+      sum += i;
+    }
+    else if (i % 3 == 0) {
+      sum += i;
+    }
+  }
+  return sum
+}
